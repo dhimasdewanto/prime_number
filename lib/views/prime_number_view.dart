3a1aad541c1c7aa9c4c50b93a25a83b5cd45a595
dhimasdewanto <dhimasbrd@gmail.com>
2021-03-29 16:29:03 +0700

    Finalize prime number view

--- lib/views/prime_number_view.dart
+++ lib/views/prime_number_view.dart
@@ -18,6 +18,29 @@ class _PrimeNumberViewState extends State<PrimeNumberView> {
   final _number2 = TextEditingController();
 
   @override
+  void dispose() { 
+    _number1.dispose();
+    _number2.dispose();
+    super.dispose();
+  }
+
+  void _onAction() {
+    FocusScope.of(context).unfocus();
+
+    final number1 = int.tryParse(_number1.text);
+    final number2 = int.tryParse(_number2.text);
+
+    if (number1 == null || number2 == null) {
+      return;
+    }
+
+    context.read<PrimeNumberController>().getPrimeNumbers(
+          number1,
+          number2,
+        );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
@@ -25,45 +48,50 @@ class _PrimeNumberViewState extends State<PrimeNumberView> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: _number1,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      labelText: "Start",
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _number1,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: "Start",
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: TextFormField(
-                    controller: _number2,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      labelText: "End",
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _number2,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: "End",
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                context.read<PrimeNumberController>().getPrimeNumbers(
-                      int.tryParse(_number1.text) ?? 0,
-                      int.tryParse(_number2.text) ?? 0,
-                    );
-              },
+              onPressed: _onAction,
               child: const Text("Get Primary Number"),
             ),
-            const SizedBox(height: 20),
-            Text(
-              context
-                  .watch<PrimeNumberController>()
-                  .state
-                  .listPrimeNumbers
-                  .toString(),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Consumer<PrimeNumberController>(
+                builder: (context, controller, child) {
+                  final listPrimeNumbers = controller.state.listPrimeNumbers;
+                  if (listPrimeNumbers.isEmpty) {
+                    return const Offstage();
+                  }
+                  return Text(
+                    listPrimeNumbers.toString(),
+                    style: Theme.of(context).textTheme.bodyText1,
+                  );
+                },
+              ),
             ),
           ],
         ),
